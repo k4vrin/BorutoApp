a09a4dea97ea48b32502f847c721a4cd33c25f0f
Mostafa Hosseini <kavrindev@gmail.com>
2022-04-28 00:30:42 +0430

    initial commit

---
+++ app/src/main/java/com/kavrin/borutoapp/navigation/NavGraph.kt
@@ -0,0 +1,39 @@
+package com.kavrin.borutoapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.kavrin.borutoapp.util.Constant
+import com.kavrin.borutoapp.util.Constant.DETAILS_ARGUMENT_KEY
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+
+        }
+        composable(route = Screen.Welcome.route) {
+
+        }
+        composable(
+            route = Screen.Home.route,
+            arguments = listOf(navArgument(DETAILS_ARGUMENT_KEY){
+                type = NavType.IntType
+            })
+        ) {
+
+        }
+        composable(route = Screen.Detail.route) {
+
+        }
+        composable(route = Screen.Search.route) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/kavrin/borutoapp/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.kavrin.borutoapp.util
+
+object Constant {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+
+}
